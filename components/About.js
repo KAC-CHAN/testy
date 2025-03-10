a216cd06fa22eefe6abc9f73b4aa6bdcc43052a1
Hritvik Ramteke <kacchan@outlook.in>
2025-03-10 14:05:08 +0530

    Create About.js

---
+++ components/About.js
@@ -0,0 +1,8 @@
+export default function AboutSection() {
+  return (
+    <section className="py-20">
+      <h2 className="text-3xl font-bold text-white mb-8">About Me</h2>
+      {/* Add your content */}
+    </section>
+  );
+}
